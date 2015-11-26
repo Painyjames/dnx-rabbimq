218894949b7a47dcbfc72ca6a312851c29c6ec4e
Victor Arbues <painyjames@gmail.com>
2015-11-26 14:28:00 +0000

    test added

---
+++ test/Components.Test/LoadTests.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Components.Messages.First;
+using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Components.Test
+{
+	public class LoadTests
+    {
+
+		private IContainer _container;
+
+		[Fact]
+        public void Component1MessageLoadTest()
+        {
+			//Arrange
+			var module = new CoreModule();
+			_container = module.Start<LoadTests>();
+			var message1 = new Message1
+			{
+				Message = "test message"
+			};
+			var numberOfMessages = 100000;
+
+			//Act
+			var producer = _container.Resolve<IProducer>();
+
+			//Assert
+			for (var i = 0; i < numberOfMessages; i++)
+			{
+				producer.Produce(message1);
+			}
+		}
+    }
+}
